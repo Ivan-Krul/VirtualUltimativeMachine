b4040fcce749a56dc3e8031204229a91e5d8a79e
Ivan-Krul <kiwiivanya128@gmail.com>
2022-10-03 20:26:42 +0300

    made a window class

--- AssemblyEditor/Window.cpp
+++ AssemblyEditor/Window.cpp
@@ -1,1 +1,51 @@
 #include "Window.h"
+
+COORD Window::getresolconsole()
+{
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	COORD Sscreenresol;
+	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+	Sscreenresol.X = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+	Sscreenresol.Y = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+	return Sscreenresol;
+}
+
+Window::Window()
+{
+	_Sresolution = getresolconsole();
+	_UIsizebuffer = _Sresolution.X * _Sresolution.Y;
+	_CPscreenbuffer = new char[_Sresolution.X * _Sresolution.Y + 1];
+	_CPscreenbuffer[_Sresolution.X * _Sresolution.Y] = '\0';
+	memset(_CPscreenbuffer, ' ', _UIsizebuffer);
+}
+
+void Window::insert(std::string Sstr_, COORD Swhere_)
+{
+	if(!(Swhere_.X < _Sresolution.X)) return;
+	if(!(Swhere_.Y < _Sresolution.Y)) return;
+	for(unsigned int Iiter = 0; Iiter < Sstr_.size(); Iiter++)
+		_CPscreenbuffer[Iiter + Swhere_.X + Swhere_.Y * _Sresolution.Y] = Sstr_[Iiter];
+}
+
+void Window::insert(char Cch_, COORD Swhere_)
+{
+	if(!(Swhere_.X < _Sresolution.X)) return;
+	if(!(Swhere_.Y < _Sresolution.Y)) return;
+	_CPscreenbuffer[Swhere_.X + Swhere_.Y * _Sresolution.Y] = Cch_;
+}
+
+char &Window::operator[](unsigned int UIindex_)
+{
+	if(!(UIindex_ < _UIsizebuffer)) return _CPscreenbuffer[0];
+	return _CPscreenbuffer[UIindex_];
+}
+
+void Window::update()
+{
+	WriteConsoleOutputCharacterA(NULL, _CPscreenbuffer, _UIsizebuffer, { 0, 0 }, NULL);
+}
+
+Window::~Window()
+{
+	delete[] _CPscreenbuffer;
+}
--- AssemblyEditor/Window.h
+++ AssemblyEditor/Window.h
@@ -1,7 +1,18 @@
 #pragma once
 #include <Windows.h>
+#include <string>
 class Window
 {
+	COORD _Sresolution;
+	unsigned int _UIsizebuffer = 0;
+	char *_CPscreenbuffer;
 
+public:
+	COORD getresolconsole();
+	Window();
+	void insert(std::string Sstr_, COORD Swhere_);
+	void insert(char Cch_, COORD Swhere_);
+	char &operator[](unsigned int UIindex_);
+	void update();
+	~Window();
 };
-
