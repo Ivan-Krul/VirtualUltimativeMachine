2e5736b90c00508744fed97887fcd63ba4690c49
Ivan-Krul <kiwiivanya128@gmail.com>
2022-10-08 21:08:24 +0300

    modify a text editor class, add stroke movement

--- AssemblyEditor/TextEditor.cpp
+++ AssemblyEditor/TextEditor.cpp
@@ -2,26 +2,53 @@
 
 void TextEditor::_curmovevert(char Cdelta_)
 {
-	/*
-		We are counting a vertical axis, every end line we decrement an iterator.
-		If we 
-	*/
-	COORD Snextcurpos = _Scurpos;
-	unsigned int UInextcurindex = _UIcurindex;
-	unsigned int UIiter = Cdelta_ < 0 ? -Cdelta_ : Cdelta_;
-	Snextcurpos.Y += Cdelta_;
-	for(UIiter; UIiter != 0; UIiter--)
-	{
-		while()
-		{
+	short SnextposY = _Scurpos.Y + Cdelta_;
+	short SnextposX = _Scurpos.X;
+	if(!(SnextposY < _VStext.size()))
+		SnextposY = _VStext.size() - 1;
+	else if(SnextposY < 0)
+		SnextposY = 0;
+	if(!(SnextposX < _VStext[SnextposY].size()))
+		SnextposX = _VStext[SnextposY].size() - 1;
+	_Scurpos = { SnextposX, SnextposY };
+}
 
-		}
-	}
+void TextEditor::_curmovehors(char Cdelta_)
+{
+	short SnextposX = _Scurpos.X + Cdelta_;
+
+	while()
 }
 
 void TextEditor::swap(char Cch_)
 {
-	_Stext[_UIcurindex] = Cch_;
+	if(Cch_ == 0x13)
+	{
+		_VStext[_Scurpos.Y][_Scurpos.X] = Cch_;
+		if(_VStext[_Scurpos.Y].capacity() == _VStext[_Scurpos.Y].size())
+			_VStext[_Scurpos.Y].reserve(_VStext[_Scurpos.Y].size() * 1.25f);
+		_VStext.push_back(std::string());
+		_Scurpos.Y++;
+		_Scurpos.X = 0;
+		if(_VStext.capacity() == _VStext.size())
+			_VStext.reserve(_VStext.capacity() * 1.25f);
+		return;
+	}
+	if(!_Bisinsertmode)
+	{
+		if(_VStext[_Scurpos.Y].capacity() == _VStext[_Scurpos.Y].size())
+			_VStext[_Scurpos.Y].reserve(_VStext[_Scurpos.Y].size() * 1.25f);
+		_VStext[_Scurpos.Y].insert(_VStext[_Scurpos.Y].begin()+_Scurpos.X, Cch_);
+		_Scurpos.X++;
+		return;
+	}
+	if(!(_Scurpos.X < _VStext[_Scurpos.Y].size()))
+	{
+		if(_VStext[_Scurpos.Y].capacity() == _VStext[_Scurpos.Y].size())
+			_VStext[_Scurpos.Y].reserve(_VStext[_Scurpos.Y].size() * 1.25f);
+		_VStext[_Scurpos.Y].insert(_VStext[_Scurpos.Y].begin() + _Scurpos.X, Cch_);
+	}
+	_VStext[_Scurpos.Y][_Scurpos.X] = Cch_;
 }
 
 void TextEditor::tabsize(unsigned char UCtabsize_)
@@ -31,4 +58,10 @@ void TextEditor::tabsize(unsigned char UCtabsize_)
 
 void TextEditor::curdown(unsigned char UCdelta_)
 {
+	_curmovevert(-UCdelta_);
+}
+
+void TextEditor::curup(unsigned char UCdelta_)
+{
+	_curmovevert(UCdelta_);
 }
--- AssemblyEditor/TextEditor.h
+++ AssemblyEditor/TextEditor.h
@@ -1,18 +1,22 @@
 #pragma once
 #include <string>
+#include <vector>
 #include <Windows.h>
 class TextEditor
 {
-	std::string _Stext = "";
+	std::vector<std::string> _VStext;
 	COORD _Scurpos = { 0,0 };
 	COORD _Sscr = { 0,0 };
-	unsigned int _UIcurindex = 0;
 	unsigned char _UCtabsize = 4;
 	bool _Bisinsertmode = false;
 	void _curmovevert(char Cdelta_);
+	void _curmovehors(char Cdelta_);
 public:
 	void swap(char Cch_);
 	void tabsize(unsigned char UCtabsize_);
 	void curdown(unsigned char UCdelta_);
+	void curup(unsigned char UCdelta_);
+	void curleft(unsigned char UCdelta_);
+	void curright(unsigned char UCdelta_);
 };
 
